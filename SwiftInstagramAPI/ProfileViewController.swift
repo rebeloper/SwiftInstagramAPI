ac4ae72c9c24670ab51368f2f65314a799f98083
Alex <therebeloper@gmail.com>
2018-12-07 11:35:11 +0200

    Initial Commit

---
+++ SwiftInstagramAPI/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  SwiftInstagramAPI
+//
+//  Created by Alex Nagy on 07/12/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
